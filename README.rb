43fe07dd4ac3f87242e8adaf9a6c711fd719d74e
RomainRec <43348150+RomainRec@users.noreply.github.com>
2018-10-04 19:26:54 +0200

    Update README.rb

--- README.rb
+++ README.rb
@@ -1,5 +1,5 @@
 Rendu des excercices 
- - 1. Les journalistes dans journalist.rb
- - 2. Cryptomonnaie dans crypto.rb
+journalist.rb (fait avec Fayssal, Hai Dai et Samuel)
+crypto.rb (fait avec Guillaume)
  
- Pseudo slack = Romain Recher
+Pseudo slack = Romain Recher
