eb23b062dda61922d96315b4dd93bb11d8f9e5ca
RomainRec <43348150+RomainRec@users.noreply.github.com>
2018-10-04 19:27:43 +0200

    Update README.rb

--- README.rb
+++ README.rb
@@ -1,5 +1,5 @@
 Rendu des excercices 
-journalist.rb (fait avec Fayssal, Hai Dai et Samuel)
-crypto.rb (fait avec Guillaume)
+journalist.rb (fait en peer avec Fayssal, Hai Dai et Samuel)
+crypto.rb (fait en peer avec Guillaume)
  
 Pseudo slack = Romain Recher
